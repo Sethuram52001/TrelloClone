7dd9622d212cc8994014189513fef2f0ce97c00d
Sethuram.S.V <58566745+Sethuram52001@users.noreply.github.com>
2020-10-28 11:48:44 +0530

    Delete index.js

--- redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-export * from "./cardActions";
-export * from "./listActions";
-
-export const CONSTANTS = {
-    ADD_CARD: "ADD_CARD",
-    ADD_LIST: "ADD_LIST",
-    REMOVE_CARD: "REMOVE_CARD",
-    DRAG_HAPPENED: "DRAG_HAPPENED"
-}
